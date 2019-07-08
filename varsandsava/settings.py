6735a979c2b8797e7351e6842cd101b02468469f
Ubuntu <ubuntu@ip-172-31-16-237.eu-west-1.compute.internal>
2019-07-08 13:34:41 +0000

    collect static

--- varsandsava/settings.py
+++ varsandsava/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5ca06f933fbb912858bb50a7ee38865aab32571ebfe1d4e2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('DEBUG') == 'True':
