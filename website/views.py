6668a31728936531608834e431f58f8ed1dde669
Ubuntu <ubuntu@ip-172-31-16-237.eu-west-1.compute.internal>
2019-07-08 14:10:45 +0000

    fixed polish language

--- website/views.py
+++ website/views.py
@@ -30,7 +30,7 @@ def en(request):
             "lang": "en",
             "title": "Vars&Sava Devs - Coming Soon",
             "text1": "Greatness in the making",
-            "text2": "Vars&Sava Devs are writing, re-writing and polishing their first site to dazzle you with the highest standard of websites! Be sure to check us out in",
+            "text2": "Vars&Sava Devs are working hard on every little detail of our first website. Be sure to check us out in",
             "text3": "September 2019",
         }
         return render(request, 'website/index.html', context)
@@ -40,7 +40,7 @@ def en(request):
             "lang": "en",
             "title": "Vars&Sava Devs - Coming Soon",
             "text1": "Greatness in the making",
-            "text2": "Vars&Sava Devs are writing, re-writing and polishing their first site to dazzle you with the highest standard of websites! Be sure to check us out in",
+            "text2": "Vars&Sava Devs are working hard on every little detail of our first website. Be sure to check us out in",
             "text3": "September 2019",
         }
         return render(request, 'website/index.html', context)
@@ -66,10 +66,10 @@ def pl(request):
         # Context for Polish version
         context = {
             "lang": "pl",
-            "title": "Vars&Sava Devs - Coming Soon",
-            "text1": "Greatness in making",
-            "text2": "Vars&Sava Devs are writing, re-writing and polishing their first site to dazzle you with the highest standard of websites! Be sure to check us out in",
-            "text3": "September 2019",
+            "title": "Vars&Sava Devs - Budujemy!",
+            "text1": "Praca wre!", 
+            "text2": "Para bucha w fabryce Vars&Sava Devs! Chuchamy i dmuchamy na nasz kod, żeby przywitać Was przepiekną stroną z naszym portfolio. Sprawdź efekt końcowy we",
+            "text3": "wrzesniu 2019!",
         }
         return render(request, 'website/index.html', context)
     
